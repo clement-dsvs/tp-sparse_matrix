5010329bc5d823861e414fea6dd9640cbe145adc
cdesavis <clement.desavis@outlook.com>
2023-04-14 15:23:57 +0200

    Initial Commit

--- TP-SparseMatrix/Source.cpp
+++ TP-SparseMatrix/Source.cpp
@@ -1,5 +1,5 @@
 #include "SparseMatrix.h"
 
 int main() {
-	SparseMatrix<int> test("input.txt");
+
 }
--- TP-SparseMatrix/SparseMatrix.h
+++ TP-SparseMatrix/SparseMatrix.h
@@ -7,7 +7,6 @@
 #include <sstream>
 #include <vector>
 
-
 struct Vector2i
 {
 	int x;
@@ -18,8 +17,25 @@ template <class T>
 class SparseMatrix
 {
 public:
+	// Constructeurs
 	SparseMatrix();
 	SparseMatrix(const char* filename);
+
+	// Modificateurs
+	void addValue(Vector2i position, T value);
+	void removeValue(Vector2i position);
+
+	T getValue(Vector2i);
+
+	// Operateurs
+	// Complexe& operator +=(const Complexe&);
+	SparseMatrix& operator +=(const SparseMatrix&);
+	SparseMatrix& operator -=(const SparseMatrix&);
+	SparseMatrix& operator *=(const SparseMatrix&);
+
+	SparseMatrix& operator +(SparseMatrix&) const;
+	SparseMatrix& operator -(SparseMatrix&) const;
+	SparseMatrix& operator *(SparseMatrix&) const;
 
 private:
 	std::map<Vector2i, T> values;
@@ -28,7 +44,7 @@ private:
 template<class T>
 SparseMatrix<T>::SparseMatrix()
 {
-	
+
 }
 
 template<class T>
@@ -49,8 +65,14 @@ SparseMatrix<T>::SparseMatrix(const char* filename)
 			tokens.push_back(line.substr(0, pos));
 			line.erase(0, pos + 1); // token + delimiter
 		}
-		
 	}
 
 	file.close();
+}
+
+template<class T>
+T SparseMatrix<T>::getValue(Vector2i position) {
+	if (auto result = values.find(position); result != values.end() {
+		return result->second;
+	}
 }
