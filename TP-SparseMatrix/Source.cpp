e28da58b0d50e46d3450eb36ceef0104d4fd0332
cdesavis <clement.desavis@outlook.com>
2023-05-26 14:24:29 +0200

    correction patron constructeur

--- TP-SparseMatrix/Source.cpp
+++ TP-SparseMatrix/Source.cpp
@@ -4,6 +4,7 @@
 int main() {
 	SparseMatrix<Complexe> m1(3, 3, "C:\\dev\\C++\\TP-SparseMatrix\\x64\\Debug\\input_complexe.txt");
 	SparseMatrix<Complexe> m2(3, 3, "C:\\dev\\C++\\TP-SparseMatrix\\x64\\Debug\\input_complexe.txt");
+	SparseMatrix<int> m3(5, 5, "C:\\dev\\C++\\TP-SparseMatrix\\x64\\Debug\\test_m1.txt");
 	m1.printToConsole();
 
 	m2 += m1;
